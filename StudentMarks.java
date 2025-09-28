083ba43bc98ee42f9901f453babd2a923b86aaba
sushantnavale05 <sushantnavale934@gmail.com>
2025-09-28 11:32:20 +0530

    First Commit

---
+++ StudentMarks.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class StudentMarks
+{
+    public static void main(String args[])
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Marks in sub1 out of (1 to 100) = ");
+        int sub1 =sc.nextInt();
+
+        System.out.println("Enter the marks in sub2 out of (1 to 100) = ");
+        int sub2 = sc.nextInt();
+
+        System.out.println("Enter the marks in sub3 put of (1 to 100) = ");
+        int sub3 = sc.nextInt();
+
+        System.out.println("Enter the marks in sub4 out of (1 to 100) = ");
+        int sub4 = sc.nextInt();
+
+        System.out.println("Enter the marks in sub5 out of (1 to 100) = ");
+        int sub5 = sc.nextInt();
+    }
+}
