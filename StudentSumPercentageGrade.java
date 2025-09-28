bd583554fa2390a02ccaa19e6e067569ab6002e4
sushantnavale05 <sushantnavale934@gmail.com>
2025-09-28 12:32:03 +0530

    Last Commit

---
+++ StudentSumPercentageGrade.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class StudentSumPercentageGrade 
+{
+    public static void main(String args[])
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Marks in sub1 out of (1 to 100) = ");
+        int sub1 =sc.nextInt();
+
+        System.out.println("Enter the marks in sub2 out of (1 to 100) = ");
+        int sub2 = sc.nextInt();
+
+        System.out.println("Enter the marks in sub3 put of (1 to 100) = ");
+        int sub3 = sc.nextInt();
+
+        System.out.println("Enter the marks in sub4 out of (1 to 100) = ");
+        int sub4 = sc.nextInt();
+
+        System.out.println("Enter the marks in sub5 out of (1 to 100) = ");
+        int sub5 = sc.nextInt();
+
+        // Calculate sun of the all marks
+        int sum = sub1 + sub2 + sub3 + sub4 + sub5;
+        System.out.println("the sun of marks in all subject is = " + sum);
+
+        // Calculate the average percentage
+        double percentage = (double)sum / 5;
+        System.out.println("The Total percentage of student is = " + percentage + "%");
+
+        // Determine the grade based on percentage
+        if(percentage >= 90)
+        {
+            System.out.println("the grade og student is = A");
+        }
+        else if(percentage >= 80)
+        {
+            System.out.println("The grade of student is = B");
+        }
+        else if(percentage >=70)
+        {
+            System.out.println("The grade of student is = C");
+        }
+        else if(percentage >= 60)
+        {
+            System.out.println("The grade of student is = D");
+        }
+        else if(percentage >= 40)
+        {
+            System.out.println("The grade of student is = E");
+        }
+        else
+        {
+            System.out.println("The grade of student is = Fail");
+        }
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Percentage = " + percentage + "%");
+        System.out.println("Grade = " + ((percentage >= 90) ? "A" : (percentage >= 80) ? "B" : (percentage >= 70) ? "C" : (percentage >=60) ? "D" : (percentage >= 40) ? "E" : "Fail"));
+    }
+    
+}
